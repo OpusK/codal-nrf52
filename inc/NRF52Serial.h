7e68f0ad9cbc5c70f7372c50dc62e3a49f964a18
Kei <kkw@robotis.com>
2020-01-31 17:08:15 +0900

    add default param value for CodalSerialDeviceProxy Class.

--- inc/NRF52Serial.h
+++ inc/NRF52Serial.h
@@ -33,7 +33,7 @@ namespace codal
          * @param rx the pin instance to use for reception
          *
          **/
-        NRF52Serial(Pin& tx, Pin& rx, NRF_UARTE_Type* uart);
+        NRF52Serial(Pin& tx, Pin& rx, NRF_UARTE_Type* uart = NRF_UARTE0);
 
         int enable();
         int disable();
